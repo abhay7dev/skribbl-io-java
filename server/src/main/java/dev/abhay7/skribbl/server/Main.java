38543a66636d07be6c8a1783083e238d266bb1d7
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2025-04-07 03:03:44 -0700

    add some arguments stuff

--- server/src/main/java/dev/abhay7/skribbl/server/Main.java
+++ server/src/main/java/dev/abhay7/skribbl/server/Main.java
@@ -2,8 +2,18 @@ package dev.abhay7.skribbl.server;
 
 public class Main {
     
+    private static int PORT = 80;
+    private static boolean HEADLESS = false;
+
     public static void main(String... args) {
-        System.out.println("Skribbl Server running!");
+        if(args.length == 1) {
+            if(args[0].equals("--headless")) HEADLESS = true;
+            else if(args[0].equals("--help")) System.out.println("Use --headless to run in headless mode. Runs on port 80.");
+            else System.err.println("Unknown argument: " + args[0]);
+        } else if(args.length > 1) {
+            System.err.println("Disallowed number of arguments (" + args.length + ")");
+        }
+        if(HEADLESS) System.out.println("Skribbl Server running on PORT " + PORT + "!");
     }
 
 }
